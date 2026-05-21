6e7ca73db9af8f582993ec28ea2622b476bf8a3b
NITIN GUPTA <SUPERFULL03213@GMAIL.COM>
2026-05-21 21:18:26 +0530

    Create project.jsx

---
+++ project.jsx
@@ -0,0 +1,19 @@
+const projects = [
+  {
+    id: 1,
+    title: "Weather App",
+    description: "Weather forecast application using API."
+  },
+  {
+    id: 2,
+    title: "Netflix Clone",
+    description: "Responsive Netflix UI clone."
+  },
+  {
+    id: 3,
+    title: "Product Catalog",
+    description: "React product filtering application."
+  }
+];
+
+export default projects;
