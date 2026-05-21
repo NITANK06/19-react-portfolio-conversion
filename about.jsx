dc4d0f10cfb2824ecb95fb6b9f652100f81ce951
NITIN GUPTA <SUPERFULL03213@GMAIL.COM>
2026-05-21 21:17:55 +0530

    Add About component with personal description

---
+++ about.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About() {
+  return (
+    <section id="about" className="section">
+      <h2>About Me</h2>
+
+      <p>
+        I am a passionate frontend developer skilled in React,
+        JavaScript, HTML, and CSS.
+      </p>
+    </section>
+  );
+}
+
+export default About;
