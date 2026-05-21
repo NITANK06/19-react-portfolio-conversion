51ed95d52a85b405fd8233328316a19d09ff5ec4
NITIN GUPTA <SUPERFULL03213@GMAIL.COM>
2026-05-21 21:17:12 +0530

    Create hero.jsx

---
+++ hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Hello, I'm Developer</h1>
+      <p>Frontend React Developer</p>
+
+      <button>View Projects</button>
+    </section>
+  );
+}
+
+export default Hero;
